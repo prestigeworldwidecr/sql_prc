dcc47c1f109e5004c9f974497c13cac00bb8ef41
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2023-02-03 11:04:34 -0600

    first commit

---
+++ test.sql
@@ -0,0 +1,10 @@
+create table test1
+(
+    Student_ID int,
+    First_Name varchar(255),
+    Last_Name varchar(255),
+    Address varchar(255),
+    City varchar(255),
+    State varchar(255),
+    Zip varchar(255),
+);
